be7fd8ecba8975a616e5a32fa77a3214f1cdf291
Alangi Derick <alangiderick@gmail.com>
2016-10-01 23:02:23 +0100

    More on variables in GoLang
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var helloWorld string = "Hello World!"
+
+	var hw string
+
+	hw = "Hello World!!"
+
+	x := "Hellow World!!!"
+
+	fmt.Println(helloWorld)
+
+	fmt.Println(hw)
+}
