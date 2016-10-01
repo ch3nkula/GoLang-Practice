2de1d41c786b9c6095b434cb13fd0a91883e5ef8
Alangi Derick <alangiderick@gmail.com>
2016-10-01 23:17:40 +0100

    Program to read user input and perform an operation
    * It reads a number from the stdin and performs a mathematical
      operation and returns the results to the stdout
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter3/userInput.go
@@ -0,0 +1,17 @@
+/* User a the main package */
+package main
+
+/* Import the input/output library */
+import "fmt"
+
+/* main function */
+func main() {
+	fmt.Print("Enter a number: ")
+	var input float64
+
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
+}
