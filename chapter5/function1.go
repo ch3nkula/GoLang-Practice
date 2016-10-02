00d957e1007d84162934ab04de4a3e7a80a20bf6
Alangi Derick <alangiderick@gmail.com>
2016-10-02 16:01:02 +0100

    Starting functions in GoLang
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter5/function1.go
@@ -0,0 +1,21 @@
+/* Functions in GoLang */
+
+package main
+
+import "fmt"
+
+func average(xs []float64) float64 {
+	panic("Not Implemented")
+}
+
+func main() {
+	xs := []float64{98, 93, 77, 82, 83}
+
+	total := 0.0
+
+	for _, v := range xs {
+		total += v
+	}
+
+	fmt.Println(total / float64(len(xs)))
+}
