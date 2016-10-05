16b8c2953be906dc71f97c57b00813208ab2890b
Alangi Derick <alangiderick@gmail.com>
2016-10-06 00:21:40 +0100

    Creating pointers with "new" built-in function in GoLang
    * Small program to utilize the creation of pointers in GoLang using
      "new()" built-in function in GoLang.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter7/pointer2.go
@@ -0,0 +1,19 @@
+/* Pointers in GoLang using the new built-in function */
+
+package main
+
+import "fmt"
+
+/* function to assign 1 to where "ptr" is pointing to */
+func one(ptr *int) {
+	*ptr = 1;
+}
+
+/* Main function */
+func main() {
+	ptr := new(int)
+
+	one(ptr)
+
+	fmt.Println("ptr points to memory address with value: ", *ptr) //ptr is 1
+}
