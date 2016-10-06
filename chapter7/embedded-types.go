004f1fdded8632e8ea2c49be2e1a074874f2b97b
Alangi Derick <alangiderick@gmail.com>
2016-10-06 02:29:45 +0100

    Adding main package and importing fmt and also assigning value to p.Name
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter7/embedded-types.go
+++ chapter7/embedded-types.go
@@ -2,11 +2,16 @@
    struct is the has-a relationship
 */
 
+ package main
+
+ import "fmt"
+
 type Person struct {
 	Name string
 }
 
 func (p *Person) Talk() {
+	p.Name = "Derick"
 	fmt.Println("Hi, my name is: ", p.Name)
 }
 
