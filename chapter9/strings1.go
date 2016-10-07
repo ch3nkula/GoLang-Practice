69d2dba6bd0467c7270ff5510e7b96cdc0b92a08
Alangi Derick <alangiderick@gmail.com>
2016-10-07 16:36:09 +0100

    Working with Strings in GoLang
    * Doing some basic string manipulation in GoLang
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter9/strings1.go
@@ -0,0 +1,46 @@
+/* Working and Manipulating strings in GoLang */
+
+package main
+
+/* Importing a list of packages */
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(
+			/* True */
+			strings.Contains("test", "es"),
+
+			/* Count is 2 */
+			strings.Count("test", "t"),
+
+			/* True */
+			strings.HasPrefix("tests", "te"),
+
+			/* True */
+			strings.HasSuffix("test", "st"),
+
+			/* Index is 1 */
+			strings.Index("test", "e"),
+
+			/* "a - b" */
+			strings.Join([]string{"a", "b"}, "-"),
+
+			/* == "aaaaa" */
+			strings.Repeat("a", 5),
+
+			/* "bbaa" */
+			strings.Replace("aaaa", "a", "b", 2),
+
+			/* []string{"a", "b", "c", "d", "e"} */
+			strings.Split("a-b-c-d-e", "-"),
+
+			/* "test" */
+			strings.ToLower("TEST"),
+
+			/* "TEST" */
+			strings.ToUpper("test"),
+		)
+}
