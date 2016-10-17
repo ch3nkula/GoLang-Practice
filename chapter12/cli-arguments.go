0a2fa68ff5531ee9b8a02583d7728b0d734898ad
Alangi Derick <alangiderick@gmail.com>
2016-10-17 23:10:29 +0100

    HTTP Servers and CLI arguments in GoLang
    * HTTP Server program to serve web pages in GoLang
      via a web socket.
    
    * CLI argument program to randomly generate a number
      between 0 to a certain max number.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter12/cli-arguments.go
@@ -0,0 +1,16 @@
+/* Handling CLI arguments with GoLang */
+
+package main
+
+import ("fmt" ; "flag" ; "math/rand")
+
+func main() {
+	/* Define the flags */
+	maxp := flag.Int("max", 6, "the max value")
+
+	/* Parse */
+	flag.Parse()
+
+	/* Generate a number between 0 and max */
+	fmt.Println(rand.Intn(*maxp))
+}
